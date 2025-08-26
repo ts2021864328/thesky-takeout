736a37a923b3d5ff740693b20e66c0e454849cd6
ts202 <ts2021864328@gmail.com>
2025-08-25 19:50:03 -0500

    分类管理模块功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -120,4 +120,33 @@ public class EmployeeController {
        return Result.success();
    }
 
+   /**
+   * 根据id查询员工信息,查询回显
+   * @param id
+   * @return
+   */
+   @ApiOperation("根据id查询员工信息")
+   @GetMapping("/{id}")
+    public Result<Employee> getById(@PathVariable Long id){
+       log.info("根据id查询员工信息：{}", id);
+
+       Employee employee = employeeService.getById(id);
+
+       return Result.success(employee);
+   }
+
+   /**
+   * 编辑员工信息
+   * @param employeeDTO
+   * @return
+   */
+   @ApiOperation("编辑员工信息")
+   @PutMapping
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+
+       log.info("编辑员工信息：{}", employeeDTO);
+       employeeService.update(employeeDTO);
+       return Result.success();
+   }
+
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -139,4 +139,34 @@ public class EmployeeServiceImpl implements EmployeeService {
         employeeMapper.update(employee);
     }
 
+    /**
+     * 根据id查询员工信息，查询回显
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public Employee getById(Long id) {
+        Employee employee = employeeMapper.getById(id);
+        employee.setPassword("****"); // 不想显示密码
+        return employee;
+
+    }
+
+    /**
+     * 编辑员工信息
+     *
+     * @param employeeDTO
+     */
+    @Override
+    public void update(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee); //属性拷贝
+
+        employee.setUpdateTime(LocalDateTime.now()); // 更新时间
+        employee.setUpdateUser(BaseContext.getCurrentId()); //更新人的id
+
+        employeeMapper.update(employee);
+    }
+
 }
